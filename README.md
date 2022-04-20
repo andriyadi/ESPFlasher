47e722603490f4428883cf776f1f45c148832fa5
Andri Yadi <an.dri@me.com>
2022-04-20 12:44:43 +0700

    Update README and comments

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ESPFlasher
- A simple component and example to flash firmware to ESP32 from.... ESP32 :)
+A simple component and example to flash firmware to ESP32 from.... ESP32 :)
+
+The project contains a component: [DxFlasher](https://github.com/andriyadi/ESPFlasher/tree/main/components/DxFlasher), that wraps [esp-serial-flasher](https://github.com/espressif/esp-serial-flasher) component due to reasons:
+* To provide C++ component
+* Make it easier to use and possibly extend to support more functionality in the future
+
+## Why?
+Why do on earth we need to flash ESP32 from anothe ESP32, you may ask?
+* Able to flash ESP32 without using computer/laptop/SBC (e.g Raspberry Pi) should be beneficial and practical, especially in the field.
+* Because we can
+
+## How to use
+Just look at [`main.cpp`](https://github.com/andriyadi/ESPFlasher/blob/main/main/main.cpp), should be self-descriptive.
+
+Especially check function of `loadHelloworldBinary`. Notes:
+* It loads example firmware which consists of 3 parts: bootloader, partition, and the app itself.
+* The actual bins are in `binaries/Hello-world/ESP32
+* Check `main/bin2array.cmake` to load another firmware.
+
+
+## Credits
+This project is not possible without this awesome component: [esp-serial-flasher](https://github.com/espressif/esp-serial-flasher)
--- main/main.cpp
+++ main/main.cpp
@@ -29,6 +29,14 @@ extern const uint32_t ESP32_hello_world_bin_size;
 extern const uint8_t  ESP32_partition_table_bin[];
 extern const uint32_t ESP32_partition_table_bin_size;
 
+/**
+ * @brief
+ * Load example firmware which consists of 3 parts: bootloader, partition, and the app itself.
+ * The actual bins are in `binaries/Hello-world/ESP32
+ * Check `main/bin2array.cmake` to load another firmware.
+ *
+ * @param bins - output binary
+ */
 static void loadHelloworldBinary(utils::DxESPFirmwareBinary_t &bins) {
     bins.boot.data = ESP32_bootloader_bin;
     bins.boot.size = ESP32_bootloader_bin_size;
@@ -45,18 +53,33 @@ static void loadHelloworldBinary(utils::DxESPFirmwareBinary_t &bins) {
 
 extern "C" void app_main(void)
 {
+    // Declare firmware binary object to flash
     utils::DxESPFirmwareBinary_t binary = {};
+    // Load it
     loadHelloworldBinary(binary);
 
-    utils::DxESPFlasherConfig_t config = {115200, GPIO_NUM_5, GPIO_NUM_4, GPIO_NUM_25, GPIO_NUM_26};
+    // Declare flasher config
+    utils::DxESPFlasherConfig_t config = {115200,
+                                          GPIO_NUM_5,
+                                          GPIO_NUM_4,
+                                          GPIO_NUM_25,
+                                          GPIO_NUM_26};
 
+    // Instantiate flasher object
     utils::DxESPFlasher flasher;
+
+    // Begin. Will reboot if it fails.
     ESP_ERROR_CHECK(flasher.begin(config));
+
+    // Connect to target MCU
     if (flasher.connectToTargetMCU(HIGHER_BAUDRATE) != ESP_LOADER_SUCCESS) {
         ESP_LOGE("APP", "Failed to connect to target MCU");
         return;
     }
+
+    // Actually flash the firmware
     ESP_ERROR_CHECK(flasher.flashFirmware(binary));
 
+    // Only shown if all is good
     ESP_LOGI("APP", "All's good");
 }
